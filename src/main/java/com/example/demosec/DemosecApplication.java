da6d4997a4fd28840ddb37b54af7240955a52882
Daniel Rodrigues da Silva <tiodanas@gmail.com>
2019-02-26 03:15:18 -0300

    first commit

---
+++ src/main/java/com/example/demosec/DemosecApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosecApplication.class, args);
+	}
+
+}
